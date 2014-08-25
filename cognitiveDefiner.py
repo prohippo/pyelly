350e96cf3df522cdd337266020122c83265e5a80
prohippo <mahc@acm.org>
2014-08-25 14:45:06 -0700

    error handling by exception

--- cognitiveDefiner.py
+++ cognitiveDefiner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # PyElly - scripting tool for analyzing natural language
 #
-# cognitiveDefiner.py : 03aug2014 CPM
+# cognitiveDefiner.py : 19aug2014 CPM
 # ------------------------------------------------------------------------------
 # Copyright (c) 2013, Clinton Prentiss Mah
 # All rights reserved.
@@ -38,6 +38,7 @@ import ellyBits
 import ellyChar
 import semanticCommand
 import featureSpecification
+import sys
 
 def convertDefinition ( stb , inp ):
 
@@ -61,13 +62,29 @@ def convertDefinition ( stb , inp ):
 #       print "cog def:",line
 
         elem = line.lower().split('>>')         # mandatory for all clauses
-        if len(elem) < 2 or len(elem[1]) == 0: return None
+        if len(elem) < 2 or len(elem[1]) == 0:
+            print >> sys.stderr , '** incomplete cognitive semantic clause'
+            return None
         left  = _leftside (stb,elem[0].strip()) # conditions for clause
         right = _rightside(stb,elem[1].strip()) # actions
-        if left == None or right == None: return None
+        if left == None or right == None:
+            return None
         store.append([ left , right ])          # save clause
         
     return store
+
+def _err ( s ):
+
+    """
+    show an error message
+    arguments:
+        s  - message string
+    returns:
+        None
+    """
+
+    print >> sys.stderr , '** cognitive semantic error:' , s
+    return None
 
 def _leftside ( stb , txt ):
 
@@ -89,18 +106,22 @@ def _leftside ( stb , txt ):
     while len(txt) > 0:
         side = txt[0]
         txt = txt[1:].lstrip()
-        if len(txt) == 0: return None
+        if len(txt) == 0:
+            return _err('malformed clause condition')
+            return None
 
         k = 0
 
         if txt[0] == '[':                   # semantic feature check?
             k = txt.find(']')               # if so, look for closing bracket
-            if k < 0: return None
+            if k < 0:
+                return _err('incomplete semantic features to check')
             p = txt[:k+1]                   # get semantic features
 
 #           print "condition:",p
             f = featureSpecification.FeatureSpecification(stb,p,'semantic')
-            if f == None: return None
+            if f == None:
+                return _err('bad semantic features')
             op = semanticCommand.Crhtf if side == 'r' else semanticCommand.Clftf
 #           print 'test:' , f.positive.hexadecimal() , f.negative.hexadecimal()
             test = ellyBits.join(f.positive,f.negative)
@@ -109,7 +130,8 @@ def _leftside ( stb , txt ):
         elif txt[0] == '(':                 # semantic concept check?
 #           print "txt=\"" + txt +"\""
             k = txt.find(')')               # if so, look for closing parenthesis
-            if k < 0: return None
+            if k < 0:
+                return _err('incomplete concept check')
             s = txt[1:k].strip().upper()    # normalize concepts
             p = s.split(',')                # allow for multiple disjunctive checks
 #           print "p=\"" + p + "\""
@@ -162,16 +184,18 @@ def _rightside ( stb , txt ):
             elif c == 'r':
                 actn.append([ semanticCommand.Crhr ])
             else:
-                return None
+                return _err('bad inheritance')
             txt = txt[2:].strip()
 
     if len(txt) > 1:
 
         if txt[0] == '[':         # set semantic features for phrase?
             n = txt.find(']')
-            if n < 0: return None
+            if n < 0:
+                return _err('incomplete semantic features to set')
             f = featureSpecification.FeatureSpecification(stb,txt[:n+1],semantic=True)
-            if f == None: return None
+            if f == None:
+                return _err('bad semantic features')
             sq = [ semanticCommand.Csetf , f ]
             txt = txt[n+1:]
 
@@ -180,7 +204,7 @@ def _rightside ( stb , txt ):
         c = txt[0]                # check for sign of plausibility change
 
         if c != '+' and c != '-':
-            return None           # must begin with + or -
+            return _err('plausibility must begin with + or -')
 
 #       print "2 txt=[",txt,"]"
 
@@ -190,15 +214,15 @@ def _rightside ( stb , txt ):
             try:
                 val = int(txt[1:])   # explicit numerical change
             except:
-                print >> sys.stderr , 'bad cognitive plausibility' , txt + ' ' + inc
-                return None
+                return _err('bad cognitive plausibility ' + txt + ' ' + str(inc))
         elif c == txt[1]:            # alternate notation for plausibility change
             val = 2
             for xc in txt[2:]:
-                if xc != c: return None
+                if xc != c:
+                    return _err('must be all + or all -')
                 val += 1             # count up value
         else:
-            return None
+            return _err('cannot interpret clause: ' + txt)
 
         if c == '-': val = -val      # get right sign
 
