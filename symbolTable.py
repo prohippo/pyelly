364a7c4267abfe372a14c8cf3caeab0568a004f2
prohippo <mahc@acm.org>
2017-03-03 20:10:14 -0800

    increase syntactic category count to 80

--- symbolTable.py
+++ symbolTable.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # PyElly - scripting tool for analyzing natural language
 #
-# symbolTable.py : 22jan2017 CPM
+# symbolTable.py : 01mar2017 CPM
 # ------------------------------------------------------------------------------
 # Copyright (c) 2013, Clinton Prentiss Mah
 # All rights reserved.
@@ -39,7 +39,7 @@ import ellyBits
 import ellyChar
 import sys
 
-NMAX = 72  # maximum number of syntactic type names
+NMAX = 80  # maximum number of syntactic type names
 FMAX = 16  # maximum number of feature names per set
 
 LAST = FMAX - 1
