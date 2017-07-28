4d18c26776df7782a397758ea2a6bb145fff780e
prohippo <mahc@acm.org>
2017-07-27 22:28:00 -0700

    clean up and extend stop punctuation exceptions

--- stopExceptions.py
+++ stopExceptions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # PyElly - scripting tool for analyzing natural language
 #
-# stopExceptions.py : 02jul2017 CPM
+# stopExceptions.py : 22jul2017 CPM
 # ------------------------------------------------------------------------------
 # Copyright (c) 2013, Clinton Prentiss Mah
 # All rights reserved.
@@ -44,15 +44,17 @@ import ellyException
 def _rejectWild ( p ):
 
     """
-    check that all pattern wildcards are acceptable
+    check that all stopException pattern wildcards are acceptable
 
     arguments:
         p  - list of pattern elements
 
     returns:
         True if any element can match other than one char, otherwise False
+        (an exception is the final element of a pattern, which can be *)
     """
 
+    if p != None and len(p) >> 0 and p[-1] == ellyWildcard.cALL: p = p[:-1]
     for pc in p:
         if ellyWildcard.isNonSimpleMatch(pc): return True
     return False
@@ -64,7 +66,7 @@ class StopExceptions(object):
 
     attributes:
         lstg  - listing of punctuation patterns as Python dictionary keyed on the punctuation
-        maxl  - maximum length of left parts of pattern
+        maxl  - maximum length of left parts of pattern excluding any final *
     """
 
     class Pattern(object):
@@ -99,8 +101,8 @@ class StopExceptions(object):
             TableFailure on error
         """
 
-        self.lstg = { }  # empty at start
-        self.maxl = 0    # maximum is zero to start with
+        self.lstg = { }    # empty at start
+        self.maxl = 0      # maximum is zero to start with
         if defs != None: self.load(defs)
 
     def load ( self , defs ):
@@ -135,20 +137,16 @@ class StopExceptions(object):
                 continue
 #           print 'ps=' , ps
 
-            idc = ps[0][-1]                # last char of left part of pattern
+            idc = ps[0][-1]                # last char of left part of pattern = possible punc
             left  = ellyWildcard.convert(ps[0][:-1])
             right = ellyWildcard.convert(ps[1])
 #           print 'idc=' , idc , 'left=' , left , 'right=' , right
-            if left == None:
+            if left  == None or _rejectWild(left):
                 print >> sys.stderr , 'bad left context in pattern' , '<' + df + '>'
                 nerr += 1
                 continue
-            if right == None or len(right) > 1:
+            if right == None or _rejectWild(right) or len(right) > 1:
                 print >> sys.stderr , 'bad right context in pattern' , '<' + df + '>'
-                nerr += 1
-                continue
-            if _rejectWild(left) or _rejectWild(right):
-                print >> sys.stderr , 'bad wildcard in pattern'
                 nerr += 1
                 continue
 
@@ -160,6 +158,8 @@ class StopExceptions(object):
 #           print 'lstg=' , self.lstg.keys()
 
             ll = len(left)
+            if ll > 0 and left[-1] == ellyWildcard.cALL:
+                ll -= 1
             if self.maxl < ll : self.maxl = ll
 
         if nerr > 0:
@@ -182,7 +182,7 @@ class StopExceptions(object):
 
 #       print 'matching for txt=' , txt , 'pnc=' , pnc , 'ctx=' , ctx
 
-        if nomatch(txt,pnc,ctx):
+        if nomatch(txt,pnc,ctx):  # preclude any exception match?
             return False
 #       print 'nomatch() returned False'
 
@@ -205,46 +205,72 @@ class StopExceptions(object):
 
 #       print 'txs= ' + unicode(txs) + ' pnc= [' + pnc + '] nxt=[' + nxt + ']'
 
-        for p in lp:              # try matching each pattern
+        ntr = 1
+        while ntr <= lt:
+            if not ellyChar.isLetterOrDigit(txs[-ntr]):
+                break
+            ntr += 1
+        txsg = txs[ntr-1-lt:]     # range for wildcard * matching
+        ltx  = len(txsg)
 
-            if p.left != None:
+        for p in lp:              # try matching each listed exception pattern
 
-                n = len(p.left)   # assume each pattern element must match one sequence char
-#               print 'n=' , n , 'p=' , unicode(p)
-                if n > lt:
-                    continue      # fail immediately because of impossibility of match
-                t = txs if n == lt else txs[-n:]
-#               print 'left pat=' , '[' + ellyWildcard.deconvert(p.left) + ']'
+            if p.left != None and len(p.left) > 0:
+
+                lp = p.left
+                star = len(lp) > 0 and lp[-1] == ellyWildcard.cALL
+                n = len(lp)       # assume each pattern element must match one sequence char
+                if star:          # except any final wildcard *
+#                   print '* ends pattern'
+                    if ltx < n - 1:
+                        continue
+                    lp = lp[:-1]
+                    t = txsg
+#                   print '[' + ellyWildcard.deconvert(lp) + ']' , t
+                else:
+#                   print 'n=' , n , 'p=' , unicode(p)
+                    if n > lt:
+                        continue  # fail immediately because of impossibility of match
+                    t = txs[-n:]
+#               print 'left pat=' , '[' + ellyWildcard.deconvert(lp) + ']'
 #               print 'versus t=' , t
-                if not ellyWildcard.match(p.left,t,0):
+                if not ellyWildcard.match(lp,t,0):
 #                   print 'no left match'
                     continue
 #               print 'left match=' , t
-                if n < lt and ellyChar.isLetterOrDigit(t[0]):
-                    if ellyChar.isLetterOrDigit(txs[-n-1]):
-                        continue  # fail because of no break in text
+                if not star:
+                    if n < lt and ellyChar.isLetterOrDigit(t[0]):
+                        if ellyChar.isLetterOrDigit(txs[-n-1]):
+                            continue  # fail because of no break in text
 
 #           nc = '\\n' if nxt == '\n' else nxt
 #           print 'right pat=' , '[' + ellyWildcard.deconvert(p.right) + ']'
 #           print 'versus c=' , nc
 
-            if p.right == []:
+            rp = p.right
+            if rp == [] or rp[0] == ellyWildcard.cALL:
                 return True
-            pcx = p.right[0]
+            pcx = rp[0]
             if pcx == nxt:                     # check for specific char after possible stop
 #               print 'right=' , nxt
                 return True
-            if pcx == ellyWildcard.cDIG:       # check for anumeric
-                if ellyChar.isLetterOrDigit(nxt):
+            elif pcx == ellyWildcard.cALF:     # check for alphabetic
+                if ellyChar.isLetter(nxt):
+#                   print 'right is alphabetic=' , nxt
+                    return True
+            elif pcx == ellyWildcard.cDIG:     # check for numeric
+                if ellyChar.isDigit(nxt):
 #                   print 'right is numeric=' , nxt
+                    return True
+            elif pcx == ellyWildcard.cUPR:     # check for upper case
+                if ellyChar.isUpperCaseLetter(nxt):
                     return True
             elif pcx == ellyWildcard.cLWR:     # check for lower case
                 if ellyChar.isLowerCaseLetter(nxt):
                     return True
-#           print 'last check'
-            if p.right == u'.':                # check for any punctuation
-                if not ellyChar.isLetterOrDigit(nxt) and not ellyChar.isWhiteSpace(nxt):
-#                   print 'right punc=' , nxt
+            elif pcx == ellyWildcard.cCAN:     # check for non-alphanumeric
+                if ellyChar.isLetter(nxt):
+#                   print 'right is alphabetic=' , nxt
                     return True
 
 #       print "no matches"
@@ -274,7 +300,7 @@ def nomatch ( txt , pnc , ctx ):
     if pnc != '.' or not ellyChar.isWhiteSpace(nxt) or ln < 5:
         return False
 #   print 'check' , txt[-3:]
-    if not txt[-1] in [ 'M' , 'm' ] or txt[-2] != '.' or not txt[-3] in [ 'P' , 'p' , 'A' , 'a' ] or txt[-4] != ' ':
+    if not txt[-1] in ['M','m'] or txt[-2] != '.' or not txt[-3] in ['P','p','A','a'] or txt[-4] != ' ':
         return False
     ch = txt[-5]
 #   print 'ch=' , ch
@@ -359,7 +385,9 @@ if __name__ == '__main__':
         list('two P.M. ') ,
         list('after six p.m. ') ,
         list('2:00. ') ,
-        list('abc.' + THS + 'def')
+        list('abc.' + THS + 'def') ,
+        list('XXX: 123') ,
+        list('XXX: Boo')
     ]
 
     nlu = len(sys.argv) - 2
@@ -381,6 +409,7 @@ if __name__ == '__main__':
                 if ku == lu or not ellyChar.isLetterOrDigit(ts[ku]):
                     break         # must not be followed by letter or digit
         else:
+            print ts , 'SKIPPED'
             continue
         res = stpx.match( ts[:ku-1] , ts[ku-1] , ts[ku:] )
         print '[ ' + ''.join(ts) + ' ] @' , ku-1 ,
